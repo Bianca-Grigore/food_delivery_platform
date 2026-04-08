526a1c0018b2746ecfcb2f88c6bd05dce0a7b8b0
Grigore <grigorebianca76@gmail.com>
2026-04-08 17:17:36 +0300

    exceptions

---
+++ src/com/pao/proiect/tema/exception/ItemUnavailableException.java
@@ -0,0 +1,7 @@
+package com.pao.proiect.tema.exception;
+
+public class ItemUnavailableException extends RuntimeException {
+    public ItemUnavailableException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/pao/proiect/tema/exception/MultipleRestaurantCartException.java
@@ -0,0 +1,7 @@
+package com.pao.proiect.tema.exception;
+
+public class MultipleRestaurantCartException extends RuntimeException {
+    public MultipleRestaurantCartException(String message) {
+        super(message);
+    }
+}
