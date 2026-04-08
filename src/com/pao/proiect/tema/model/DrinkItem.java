a59a2e8dcdc6f4d169d40c39b831224a25f821f8
Grigore <grigorebianca76@gmail.com>
2026-04-08 17:12:34 +0300

    DrinkItem

--- src/com/pao/proiect/tema/model/DrinkItem.java
+++ src/com/pao/proiect/tema/model/DrinkItem.java
@@ -1,4 +1,51 @@
 package com.pao.proiect.tema.model;
 
 public non-sealed class DrinkItem extends MenuItem {
+    private boolean containsAlcohol;
+    private double alcoholPercentage;
+    private int volumeMl;
+    private boolean isHot;
+
+    public DrinkItem(String name, double calories, double price, String description, int estimatedTime, boolean isVegan, boolean isVegetarian, boolean containsAlcohol, int volumeMl,  boolean isHot, double alcoholPercentage){
+        super(name, calories, price, description, estimatedTime, isVegan, isVegetarian);
+       this.containsAlcohol = containsAlcohol;
+       this.volumeMl = volumeMl;
+       this.isHot= isHot;
+       this.alcoholPercentage = containsAlcohol ? alcoholPercentage : 0.0;
+    }
+
+    public boolean requiresId(){
+        return containsAlcohol;
+    }
+
+    public double getAlcoholPercentage(){
+        if(!this.containsAlcohol || this.alcoholPercentage <= 0){
+            return 0.0;
+        }
+        return (this.volumeMl * this.alcoholPercentage) / 1000.0;
+    }
+
+    public boolean addIced(){
+        if(this.isHot){
+            this.isHot = false;
+            this.name = "Iced " + this.name;
+            return true;
+        }
+        return false;
+    }
+
+    public void upsize(int extra){
+        if(extra > 0){
+            double p = (double) (this.volumeMl + extra)/this.volumeMl;
+            this.volumeMl += extra;
+            this.calories += p;
+            this.price *= (1+((p-1)*0.8));
+            System.out.println("Drink upsized by " + extra + "ml. New volume: " + this.volumeMl + "ml, new calories: " + this.calories + ", new price: " + this.price);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", volume: " + volumeMl + ", alcohol percentage: " + alcoholPercentage + "%, hot drink: " + isHot;
+    }
 }
--- src/com/pao/proiect/tema/model/FoodItem.java
+++ src/com/pao/proiect/tema/model/FoodItem.java
@@ -1,14 +1,27 @@
 package com.pao.proiect.tema.model;
 
-import java.util.Set;
 
 public non-sealed class FoodItem extends MenuItem {
     private double weightGrams;
     private Spiciness spicinessLevel;
     private CourseType courseType;
 
+    public FoodItem(String name, double calories, double price, String description, int estimatedTime, boolean isVegan, boolean isVegetarian, double weightGrams, Spiciness spicinessLevel, CourseType courseType){
+        super(name, calories, price, description, estimatedTime, isVegan, isVegetarian);
+        this.weightGrams = weightGrams;
+        this.spicinessLevel = spicinessLevel;
+        this.courseType = courseType;
+    }
 
+    public double getCaloriesPer100(){
+        if(this.weightGrams <= 0){
+            return 0;
+        }
+        return (this.calories/this.weightGrams) * 100;
+    }
 
-
-
+    @Override
+    public String toString(){
+        return super.toString() + ", grams: " + weightGrams + ", spiciness: " + spicinessLevel + ", course type: " + courseType;
+    }
 }
