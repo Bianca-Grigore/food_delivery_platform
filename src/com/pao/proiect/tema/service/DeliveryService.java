3df6e9a71d7474b111451d9ffc1f5f976b65f0e6
Grigore <grigorebianca76@gmail.com>
2026-04-09 21:14:11 +0300

    DeliveryService

--- src/com/pao/proiect/tema/service/DeliveryService.java
+++ src/com/pao/proiect/tema/service/DeliveryService.java
@@ -1,4 +1,21 @@
 package com.pao.proiect.tema.service;
 
+import com.pao.proiect.tema.model.DeliveryPerson;
+
 public class DeliveryService {
+    private DeliveryPerson[] drivers;
+
+    private DeliveryService(){
+        this.drivers = new DeliveryPerson[0];
+    }
+
+    private static class Holder{
+        private static final DeliveryService INSTANCE = new DeliveryService();
+    }
+
+    public static DeliveryService getInstance(){
+        return DeliveryService.Holder.INSTANCE;
+    }
+
+
 }
