3bef3fb97ff6a68f3e6607d146ded06202e4cb06
Grigore <grigorebianca76@gmail.com>
2026-04-15 19:19:12 +0300

    instances

--- src/com/pao/proiect/tema/service/MenuService.java
+++ src/com/pao/proiect/tema/service/MenuService.java
@@ -1,4 +1,22 @@
 package com.pao.proiect.tema.service;
 
+import com.pao.proiect.tema.model.Menu;
+import com.pao.proiect.tema.model.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuService {
+    private List<MenuItem> menu;
+    private MenuService(){
+        this.menu = new ArrayList<>();
+    }
+
+    private static class Holder{
+        private static final MenuService INSTANCE = new MenuService();
+    }
+
+    public static MenuService getInstance(){
+        return MenuService.Holder.INSTANCE;
+    }
 }
--- src/com/pao/proiect/tema/service/UserService.java
+++ src/com/pao/proiect/tema/service/UserService.java
@@ -1,4 +1,19 @@
 package com.pao.proiect.tema.service;
 
+import com.pao.proiect.tema.model.User;
+
 public class UserService {
+    private User[] users;
+
+    private UserService() {
+        this.users = new User[0];
+    }
+
+    private static class Holder{
+        private static final UserService INSTANCE = new UserService();
+    }
+
+    public static UserService getInstance(){
+        return UserService.Holder.INSTANCE;
+    }
 }
