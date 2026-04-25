c5fef8d92e5b51f00400cb952f1b437d03169095
Grigore <grigorebianca76@gmail.com>
2026-04-25 19:06:00 +0300

    sterse

--- src/com/pao/proiect/tema/exception/AgeRestrictionException.java
+++
@@ -1,7 +0,0 @@
-package com.pao.proiect.tema.exception;
-
-public class AgeRestrictionException extends RuntimeException {
-    public AgeRestrictionException(String message) {
-        super(message);
-    }
-}
