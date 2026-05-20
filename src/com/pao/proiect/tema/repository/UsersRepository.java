5080921a514bad34b6519e1f4664ea1c85b94d3d
Grigore <grigorebianca76@gmail.com>
2026-05-20 19:13:50 +0300

    user repository complete

--- src/com/pao/proiect/tema/repository/UsersRepository.java
+++ src/com/pao/proiect/tema/repository/UsersRepository.java
@@ -3,9 +3,8 @@ import com.pao.proiect.tema.model.*;
 import com.pao.proiect.tema.util.DatabaseConnection;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -61,26 +60,129 @@ public class UsersRepository implements Repository<User, Integer>{
 
     @Override
     public void save(User entity) throws SQLException {
+        String sql = "INSERT INTO users (name, email, phone_num, password, role, city, street, building_number, postal_code, details, points, restaurant_name, is_active, access_level, vehicle_type, is_available, rating, total_deliveries) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try(PreparedStatement ps = getConn().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getEmail());
+            ps.setString(3, entity.getPhoneNum());
+            ps.setString(4, entity.getPassword());
+            ps.setString(5, entity.getRole());
 
+            for(int i=6; i<=18; i++){
+                ps.setObject(i, null);
+            }
+
+            if(entity instanceof Customer customer){
+                Address address = customer.getAddress();
+                ps.setString(6, address.city());
+                ps.setString(7, address.street());
+                ps.setString(8, address.buildingNumber());
+                ps.setString(9, address.postalCode());
+                ps.setString(10, address.details());
+                ps.setInt(11, customer.getPoints());
+
+            }
+            else
+                if(entity instanceof RestaurantAdmin admin){
+                    ps.setString(12, admin.getRestaurantName());
+                    ps.setBoolean(13, admin.isActive());
+                    ps.setString(14, admin.getAccessLevel().name());
+                }
+                else if(entity instanceof DeliveryPerson deliveryPerson){
+                    ps.setString(15, deliveryPerson.getVehicleType().name());
+                    ps.setBoolean(16, deliveryPerson.isAvailable());
+                    ps.setDouble(17, deliveryPerson.getRating());
+                    ps.setInt(18, deliveryPerson.getTotalDeliveries());
+                }
+                ps.executeUpdate();
+                try(ResultSet keys = ps.getGeneratedKeys()){
+                    if(keys.next())
+                        entity.setId(keys.getInt(1));
+                }
+        }catch(IOException e){
+            throw new SQLException(e);
+        }
     }
 
     @Override
-    public Optional<User> findById(Integer aInteger) throws SQLException {
-        return Optional.empty();
+    public Optional<User> findById(Integer id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id= ?";
+        try(PreparedStatement ps = getConn().prepareStatement(sql)){
+            ps.setInt(1, id);
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()) return Optional.of(mapRow(rs));
+                return Optional.empty();
+            }
+        }catch (IOException e){
+            throw new SQLException(e);
+        }
     }
 
     @Override
     public List<User> findAll() throws SQLException {
-        return List.of();
+        String sql = "SELECT * FROM users ORDER BY id";
+        List<User> list = new ArrayList<>();
+        try(PreparedStatement ps = getConn().prepareStatement(sql); ResultSet rs = ps.executeQuery()){
+            while(rs.next())
+                list.add(mapRow(rs));
+        }catch (IOException e){
+            throw new SQLException(e);
+        }
+        return list;
     }
 
     @Override
     public void update(User entity) throws SQLException {
+        String sql = "UPDATE users SET name=?, email=?, phone_num=?, password=?, role=?, city=?, street=?, building_number=?, postal_code=?, details=?, points=?, restaurant_name=?, is_active=?, access_level=?, vehicle_type=?, is_available=?, rating=?, total_deliveries=? WHERE id=?";
+        try(PreparedStatement ps = getConn().prepareStatement(sql)){
+            ps.setString(1, entity.getName());
+            ps.setString(2, entity.getEmail());
+            ps.setString(3, entity.getPhoneNum());
+            ps.setString(4, entity.getPassword());
+            ps.setString(5, entity.getRole());
+
+            for(int i=6; i<=18; i++){
+                ps.setObject(i, null);
+            }
+
+            if(entity instanceof Customer customer){
+                Address address = customer.getAddress();
+                ps.setString(6, address.city());
+                ps.setString(7, address.street());
+                ps.setString(8, address.buildingNumber());
+                ps.setString(9, address.postalCode());
+                ps.setString(10, address.details());
+                ps.setInt(11, customer.getPoints());
+
+            }
+            else
+            if(entity instanceof RestaurantAdmin admin){
+                ps.setString(12, admin.getRestaurantName());
+                ps.setBoolean(13, admin.isActive());
+                ps.setString(14, admin.getAccessLevel().name());
+            }
+            else if(entity instanceof DeliveryPerson deliveryPerson){
+                ps.setString(15, deliveryPerson.getVehicleType().name());
+                ps.setBoolean(16, deliveryPerson.isAvailable());
+                ps.setDouble(17, deliveryPerson.getRating());
+                ps.setInt(18, deliveryPerson.getTotalDeliveries());
+            }
+            ps.setInt(19, entity.getId());
+            ps.executeUpdate();
+        }catch(IOException e){
+            throw new SQLException(e);
+        }
 
     }
 
     @Override
-    public void delete(Integer aInteger) throws SQLException {
-
+    public void delete(Integer id) throws SQLException {
+        String sql = "DELETE FROM users WHERE id= ?";
+        try(PreparedStatement ps = getConn().prepareStatement(sql)){
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }catch(IOException e){
+            throw new SQLException(e);
+        }
     }
 }
